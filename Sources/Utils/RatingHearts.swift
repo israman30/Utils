506e5cf855417beff3be3e0bd68667254cd06791
Israel Manzo <israelmanzo814@gmail.com>
2025-09-15 23:16:44 -0400

    added rating hearts

---
+++ Sources/Utils/RatingHearts.swift
@@ -0,0 +1,44 @@
+//
+//  SwiftUIView.swift
+//  Utils
+//
+//  Created by Israel Manzo on 9/15/25.
+//
+
+import SwiftUI
+
+public struct RatingHeartsView: View {
+    var rating: CGFloat
+    var maxRating: Int
+
+    public var body: some View {
+        hearts
+            .overlay(
+            GeometryReader {
+                let width = rating / CGFloat(maxRating) * $0.size.width
+                ZStack(alignment: .leading) {
+                    Rectangle()
+                        .frame(width: width)
+                        .foregroundColor(.red)
+                }
+            }
+            .mask(hearts)
+        )
+        .foregroundColor(.gray)
+    }
+    
+    var hearts: some View {
+        HStack(spacing: 0) {
+           ForEach(0..<maxRating, id: \.self) { _ in
+               Image(systemName: "heart.fill")
+                   .resizable()
+                   .aspectRatio(contentMode: .fit)
+           }
+       }
+    }
+}
+
+#Preview {
+    RatingHeartsView(rating: 1.5, maxRating: 5)
+}
+
