8a39c1f3b1f496f42aad010aece3a19b2a99dd41
Israel Manzo <israelmanzo814@gmail.com>
2025-09-16 10:24:16 -0400

    added stepper

---
+++ Sources/Utils/Stepper.swift
@@ -0,0 +1,41 @@
+//
+//  SwiftUIView.swift
+//  Utils
+//
+//  Created by Israel Manzo on 9/16/25.
+//
+
+import SwiftUI
+
+struct StepperView: View {
+    @State var value = 0
+    var body: some View {
+        StepperViewUtils(title: "stepper", value: $value, min: 0, max: 100, step: 1) {
+            // update
+        }
+    }
+}
+
+#Preview {
+    StepperView()
+}
+
+public struct StepperViewUtils: View {
+    var title: String = ""
+    @Binding var value: Int
+    let min: Int
+    let max: Int
+    let step: Int
+    let onUpdate:(() -> Void)?
+    
+    public var body: some View {
+        if #available(iOS 17.0, *) {
+            Stepper(title, value: $value, step: step)
+                .onChange(of: value) { _,_ in
+                    onUpdate?()
+                }
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+}
