e7fd36c3951046a2fa33779be4eb2894c769c4b9
Israel Manzo <israelmanzo814@gmail.com>
2025-09-15 23:14:38 -0400

    added rating star view

---
+++ Sources/Utils/RatingStar.swift
@@ -0,0 +1,42 @@
+//
+//  SwiftUIView.swift
+//  Utils
+//
+//  Created by Israel Manzo on 9/15/25.
+//
+
+import SwiftUI
+
+public struct RatingStarsView: View {
+    var rating: CGFloat
+    var maxRating: Int
+
+    public var body: some View {
+        stars
+            .overlay(
+            GeometryReader {
+                let width = rating / CGFloat(maxRating) * $0.size.width
+                ZStack(alignment: .leading) {
+                    Rectangle()
+                        .frame(width: width)
+                        .foregroundColor(.yellow)
+                }
+            }
+            .mask(stars)
+        )
+        .foregroundColor(.gray)
+    }
+    
+    var stars: some View {
+        HStack(spacing: 0) {
+           ForEach(0..<maxRating, id: \.self) { _ in
+               Image(systemName: "star.fill")
+                   .resizable()
+                   .aspectRatio(contentMode: .fit)
+           }
+       }
+    }
+}
+#Preview {
+    RatingStarsView(rating: 3.5, maxRating: 5)
+}
