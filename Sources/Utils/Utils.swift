ba136ac583b50b780d7c20482ec038a210b4ffa9
Israel Manzo <israelmanzo814@gmail.com>
2025-09-15 23:02:21 -0400

    Added hearth like component

--- Sources/Utils/Utils.swift
+++ Sources/Utils/Utils.swift
@@ -3,16 +3,49 @@
 
 import SwiftUI
 
-public class MyClass {
-    public func fizBuzz() -> String {
-        return "Hello, World!"
+public struct HeartLikeView: View {
+    
+    @Binding var isLiked: Bool
+    @State var animationAmount = 1.0
+    
+    private let animationDuration = 0.1
+    
+    private var animationScale: CGFloat {
+        isLiked ? 0.7 : 1.3
     }
     
-    public func anotherMethod() -> String {
-        return "Another method"
+    @State private var isAnimating = false
+    
+    public var body: some View {
+        Button {
+            self.executeButtonAnimation()
+        } label: {
+            VStack {
+                if #available(iOS 15.0, *) {
+                    Image(systemName: isLiked ? "heart.fill" : "heart")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100, height: 100)
+                        .foregroundStyle(isLiked ? .red : .gray)
+                } else {
+                    // Fallback on earlier versions
+                }
+            }
+            .scaleEffect(isAnimating ? animationScale : 1)
+            .animation(.easeInOut(duration: animationDuration), value: animationDuration)
+        }
     }
     
-    public func yetAnotherMethod() -> String {
-        return "Yet another method"
+    private func executeButtonAnimation() {
+        self.isAnimating = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + animationDuration) {
+            self.isAnimating = false
+            self.isLiked.toggle()
+        }
     }
 }
+
+#Preview {
+    HeartLikeView(isLiked: .constant(false))
+}
+
