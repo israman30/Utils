fab7885afc25248db5c26e05cadb10c5595d3c27
Israel Manzo <israelmanzo814@gmail.com>
2026-07-02 23:06:47 -0400

    add toolbar builder + toolbar button

--- Sources/Utils/ToolbarView.swift
+++ Sources/Utils/ToolbarView.swift
@@ -33,3 +33,46 @@ struct ToolbarView: View {
 #Preview {
     ToolbarView()
 }
+
+// MARK: - Reusable Toolbar Item
+struct ToolbarItemBuilder<Content: View>: ToolbarContent {
+    let placement: ToolbarItemPlacement
+    let content: () -> Content
+    
+    init(placement: ToolbarItemPlacement, @ViewBuilder content: @escaping () -> Content) {
+        self.placement = placement
+        self.content = content
+    }
+    
+    var body: some ToolbarContent {
+        ToolbarItem(placement: placement, content: content)
+    }
+}
+
+// MARK: - Convenience Builders
+public struct ToolbarButton: ToolbarContent {
+    let placement: ToolbarItemPlacement
+    let icon: String?
+    let label: String?
+    let action: () -> Void
+    init(placement: ToolbarItemPlacement, icon: String?, label: String?, action: @escaping () -> Void) {
+        self.placement = placement
+        self.icon = icon
+        self.label = label
+        self.action = action
+    }
+    
+    public var body: some ToolbarContent {
+        ToolbarItem(placement: placement) {
+            Button(action: action) {
+                if let icon = icon, let label = label {
+                    Label(label, systemImage: icon)
+                } else if let icon = icon {
+                    Image(systemName: icon)
+                } else if let label = label {
+                    Text(label)
+                }
+            }
+        }
+    }
+}
